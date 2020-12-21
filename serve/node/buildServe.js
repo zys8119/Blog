ffabbb92c063893d287cc5c2051ff8400092a805
zys8119 <770959294@qq.com>
2020-12-21 09:42:54 +0800

    自动化部署及钉钉消息推送

--- serve/node/buildServe.js
+++ serve/node/buildServe.js
@@ -3,6 +3,73 @@ const { execSync } = require("child_process");
 const { resolve } = require("path");
 const fs = require("fs");
 const path = require("path");
+const axios = require('axios');
+const crypto = require('crypto');
+const lodash = require('lodash');
+/**
+ * 默认配置
+ * */
+const defaultOptions = {
+    // 本地git部署项目目录
+    cwd:"H:\\work\\common_prod_rd",
+    // 相对具体部署目录
+    dir:"./npc_h5/administrative-finance",
+    // 发布分支
+    branch:"release",
+    // 文件拷贝目标
+    copyPath:resolve(__dirname,"../../dist"),
+    // 消息
+    info:{
+        title:"测试醒目",
+        setting:"测试",
+        dir:"common_prod_rd",
+        remark:"common_prod_rd",
+    },
+    // 钉钉机器人配置
+    dingtalk:{
+        // 机器人token
+        access_token:null,
+        // 机器人密钥
+        secret:null,
+        // 钉钉消息模板
+        getContent:options=>{
+            return `前端更新\n1、更新项目名称：${options.info.title}\n2、更新环境：${options.info.setting}\n3、更新目录标明${options.info.dir} ${options.branch} 分支\n4、更新内容说明（回滚使用）：${commit}\n5、备注说明：${options.info.remark}`
+        }
+    },
+    // 具体配置请参考钉钉开发文档：https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
+    description:{
+        // 消息类型
+        msgtype: "text",
+        text: {
+            // 默认会被getContent替代，所以暂不建议使用
+            content:""
+        },
+        at:{
+            // 发送并提醒给谁
+            atMobiles:[
+            ],
+            isAtAll:false,
+        }
+    },
+};
+// 合并配置
+let customConfig = {};
+let type = process.argv[process.argv.indexOf("--config")+1];
+if(type){
+    try {
+        const buildServeConfig = require('../../buildServeConfig.js');
+        if(buildServeConfig && buildServeConfig[type]){
+            customConfig = buildServeConfig[type];
+        }
+    }catch (e){
+        ncol.color(function (){this.error(e);})
+    }
+}
+const options = lodash.merge(defaultOptions, customConfig);
+
+/**
+ * 控制台交互输入
+ */
 class input{
     options = null;
     log = ncol;
@@ -49,7 +116,11 @@ class input{
     }
 }
 
-
+/**
+ * 删除文件夹
+ * @param filePath
+ * @returns {Promise<unknown>|Promise<void>}
+ */
 function rmdirPromise(filePath) {
     if(!fs.existsSync(filePath)){
         return Promise.resolve();
@@ -86,37 +157,41 @@ function rmdirPromise(filePath) {
         })
     })
 }
-const options = {
-    cwd:"H:\\work\\common_prod_rd",
-    dir:"./npc_h5/administrative-finance",
-    branch:"release",
-    copyPath:resolve(__dirname,"../../dist"),
-    info:{
-        title:"宁波人大新版",
-        setting:"测试",
-        dir:"common_prod_rd",
-        remark:"common_prod_rd",
-    },
-    description:{
-        msgtype: "text",
-        text: {
-            content:""
+
+
+/**
+ * 钉钉机器人消息发送
+ * 开发文档：https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
+ */
+function dingtalkSend(){
+    let timestamp = Date.now();
+    let access_token = options.dingtalk.access_token;
+    const secret = options.dingtalk.secret;
+    const sign = crypto
+        .createHmac('sha256', secret)
+        .update(`${timestamp}\n${secret}`, "utf8")
+        .digest('base64');
+    axios({
+        url:"https://oapi.dingtalk.com/robot/send",
+        method:"post",
+        params:{
+            access_token,
+            timestamp,
+            sign:encodeURIComponent(sign),
         },
-        at:{
-            atMobiles:[
-                "17858938961",// 群昵称：秦慧桦(秦慧桦)
-                "13857483191",// 群昵称：陈周云(陈周云)
-            ],
-            isAtAll:true,
-        }
-    }
-};
+        data:options.description
+    })
+}
+
+
 //*
+ncol.color(function (){this.info("拉取最新代码");})
+console.log(execSync(`git pull`,{cwd:options.cwd}).toString())
 ncol.color(function (){this.log("却换到").info(options.branch).log("分支");})
 console.log(execSync(`git checkout ${options.branch}`,{cwd:options.cwd}).toString())
 ncol.color(function (){this.info("当前分支");})
 console.log(execSync(`git branch`,{cwd:options.cwd}).toString())
-ncol.color(function (){this.info("拉取最新代码");})
+ncol.color(function (){this.info("同步分支最新代码");})
 console.log(execSync(`git pull`,{cwd:options.cwd}).toString())
 ncol.color(function (){this.info("复制打包文件");})
 const dirName = options.dir.split("/").filter(e=>e).pop()
@@ -139,7 +214,7 @@ rmdirPromise(resolve(options.cwd, options.dir)).then(() => {
                 ncol.color(function (){this.error("commit 提交内容为必填");})
                 return ;
             }
-            options.description.text.content = `前端更新\n1、更新项目名称：${options.info.title}\n2、更新环境：${options.info.setting}\n3、更新目录标明${options.info.dir} ${options.branch} 分支\n4、更新内容说明（回滚使用）：${commit}\n5、备注说明：${options.info.remark}`
+            options.description.text.content = options.dingtalk.getContent(options,commit);
             ncol.color(function (){this.success(execSync(`git commit -m \'${commit}\'`,{cwd:options.cwd}).toString());})
             ncol.color(function (){this.info(execSync(`git push origin ${options.branch}`,{cwd:options.cwd}).toString());})
             ncol.color(function (){this.success("提交完成");})
@@ -153,29 +228,3 @@ rmdirPromise(resolve(options.cwd, options.dir)).then(() => {
     }
 })
 //*/
-/**
- * 钉钉机器人消息发送
- * 开发文档：https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
- */
-function dingtalkSend(){
-    let timestamp = Date.now();
-    let access_token = "4fd7f9fed237f5ac5b72ab1ac32d069d13532a8e44aaaa4a212c0551e21df981";
-    const axios = require('axios');
-    const crypto = require('crypto');
-    const secret = "SEC949de5da0d2656f474e606de59d76a1e121bef515b8d9897c8620c9171744594";
-    const sign = crypto
-        .createHmac('sha256', secret)
-        .update(`${timestamp}\n${secret}`, "utf8")
-        .digest('base64');
-    axios({
-        url:"https://oapi.dingtalk.com/robot/send",
-        method:"post",
-        params:{
-            access_token,
-            timestamp,
-            sign:encodeURIComponent(sign),
-        },
-        data:options.description
-    })
-}
-
