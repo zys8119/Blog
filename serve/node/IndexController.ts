b8820783176a94ecf6d47760834857ab67053f09
zys8119 <770959294@qq.com>
2022-11-21 13:19:53 +0800

    [add] IndexController.ts

--- serve/node/IndexController.ts
+++ serve/node/IndexController.ts
@@ -33,7 +33,7 @@ export class IndexController extends applicationController {
             sub_host:'https://sub.789.st/sub',// 订阅地址域名host
             selector:'#main-container > div > div > div > div > div > div > div:nth-child(3) > div',
             // 判断是否登陆的选择器
-            selector_is_login:"#main-container > div > div.block.block-rounded.mb-2 > div > div > div.p-1.p-md-3.col-md-6.col-xs-12.text-md-right > a.btn.btn-sm.btn-primary.btn-rounded.px-3.mr-1.my-1.ant-dropdown-trigger",
+            selector_is_login:"#main-container .ant-dropdown-trigger",
         }, this.$_query)
         await this.giaoyunLogin(data)
     }
@@ -50,36 +50,50 @@ export class IndexController extends applicationController {
     }){
         console.log("尝试注册")
         const browser = await puppeteer.launch({
+            // liunx部署 需要以下配置
+            // executablePath: '/usr/bin/chromium',
+            // args: ["--no-sandbox", "--disable-setuid-sandbox"]
         })
         try {
             const page = await browser.newPage();
+            page.once("domcontentloaded", async ()=>{
+                console.log("检查表单元素是否存在")
+                await page.waitForSelector(`${selector}:nth-child(1) input`)
+                console.log("正在写入注册数据")
+                await page.type(`${selector}:nth-child(1) input`, email)
+                await page.type(`${selector}:nth-child(2) input`, password)
+                await page.type(`${selector}:nth-child(3) input`, password)
+                await page.type(`${selector}:nth-child(4) input`, invite_code)
+                await page.tap(`${selector}:nth-child(5) button`)
+                console.log("正在等待注册成功")
+                await new Promise(resolve => setTimeout(()=>resolve(null), 500))
+                console.log("注册成功并登陆成功")
+                console.log("获取订阅数据中")
+                const resultHandle = await page.evaluateHandle( (host)=>{
+                    return fetch(`${host}/api/v1/user/getSubscribe`).then(res=>res.json())
+                }, host)
+                const res = await resultHandle.jsonValue()
+                const subscribe_url = res?.data?.subscribe_url
+                console.log("获取订阅数据：", JSON.stringify(res))
+                console.log("订阅链接：", subscribe_url)
+                const clashSubscribeUrl = sub_host ? `${sub_host}?target=clash&url=${encodeURIComponent(subscribe_url)}` : subscribe_url
+                console.log("clash订阅链接转换地址：", clashSubscribeUrl)
+                const subscribeInfo = await (await page.evaluateHandle((clashSubscribeUrl)=>{
+                    return fetch(clashSubscribeUrl).then(res=>res.text())
+                }, clashSubscribeUrl)).jsonValue()
+                await browser.close()
+                this.response.writeHead(200,{
+                    'Content-Type':'text/plain; charset=UTF-8',
+                    'access-control-allow-origin':'*',
+                });
+                this.response.end(subscribeInfo)
+            })
+            page.on("error",async (e)=>{
+                console.error(e)
+                await browser.close()
+                this.$_error()
+            })
             await page.goto(`${host}/#/register`)
-            console.log("正在写入注册数据")
-            await page.type(`${selector}:nth-child(1) input`, email)
-            await page.type(`${selector}:nth-child(2) input`, password)
-            await page.type(`${selector}:nth-child(3) input`, password)
-            await page.type(`${selector}:nth-child(4) input`, invite_code)
-            await page.tap(`${selector}:nth-child(5) button`)
-            console.log("正在等待注册成功")
-            await page.waitForSelector(selector_is_login)
-            console.log("注册成功并登陆成功")
-            console.log("获取订阅数据中")
-            const resultHandle = await page.evaluateHandle( (host)=>{
-                return fetch(`${host}/api/v1/user/getSubscribe`).then(res=>res.json())
-            }, host)
-            const res = await resultHandle.jsonValue()
-            const subscribe_url = res?.data?.subscribe_url
-            console.log("获取订阅数据：", JSON.stringify(res))
-            console.log("订阅链接：", subscribe_url)
-            const subscribeInfo = await (await page.evaluateHandle((subscribe_url,sub_host)=>{
-                return fetch(sub_host ? `${sub_host}?target=clash&url=${encodeURIComponent(subscribe_url)}` : subscribe_url).then(res=>res.text())
-            }, subscribe_url, sub_host)).jsonValue()
-            await browser.close()
-            this.response.writeHead(200,{
-                'Content-Type':'text/plain; charset=UTF-8',
-                'access-control-allow-origin':'*',
-            });
-            this.response.end(subscribeInfo)
         }catch (e){
             console.error(e)
             await browser.close()
