c7384a50e276953696542b985ffd4c6b267f98c4
zys8119 <770959294@qq.com>
2022-12-06 10:40:11 +0800

    [add] vue3-form-submit.md

--- web/canvas/animation.ts
+++ web/canvas/animation.ts
@@ -1,24 +1,51 @@
-export default (callback:Function, timeout:number)=>new Promise((resolve, reject)=>{
+const animationFn = (callback:(progress:number)=>boolean | void, timeout:number, sync?:boolean, resolve?:any, reject?:any) => {
     try {
-        let ra = null;
-        let startTime = Date.now();
-        let endTime = startTime+timeout;
-        const fn = ()=>{
-            let progress = Number(((timeout - (endTime - startTime))/timeout));
-            if(progress < 0){progress = 0}
-            if(progress > 1){progress = 1}
-            callback(progress)
-            if(progress >= 1){
-                cancelAnimationFrame(ra);
+        let isReturn = false
+        let ra:any = null
+        let startTime = Date.now()
+        const endTime = startTime + (timeout === 0 ? Infinity : timeout)
+        const fn = () => {
+            let progress = Number(((timeout - (endTime - startTime)) / timeout))
+            if (progress < 0) {progress = 0}
+            if (progress > 1) {progress = 1}
+            isReturn = callback(progress) as boolean
+            if (progress >= 1) {
+                if (!sync) {
+                    cancelAnimationFrame(ra)
+                }
                 resolve(progress)
-                return;
+                return
             }
-            startTime = Date.now();
-            ra = requestAnimationFrame(fn);
+            startTime = Date.now()
+            if (sync) {
+                if (!isReturn) {
+                    ra = fn()
+                }
+            } else {
+                ra = requestAnimationFrame(fn)
+            }
         }
-        callback(0)
-        ra = requestAnimationFrame(fn)
-    }catch (e) {
+        isReturn = callback(0) as boolean
+        if (sync) {
+            if (!isReturn) {
+                ra = fn()
+            }
+        } else {
+            ra = requestAnimationFrame(fn)
+        }
+    } catch (e) {
         reject(e)
     }
-})
+}
+const animation = (callback:(progress:number)=>boolean | void, timeout:number, sync?:boolean) => {
+    if (sync) {
+        animationFn(callback, timeout, sync, () => {}, (e:Error) => {
+            throw e
+        })
+    } else {
+        return new Promise((resolve, reject) => {
+            animationFn(callback, timeout, sync, resolve, reject)
+        })
+    }
+}
+export default animation
