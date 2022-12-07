036d6d3be601e8f8d204ad6d040de8aa7de35771
zys8119 <770959294@qq.com>
2022-12-07 15:44:52 +0800

    [add] 鼠标拖拽坐标捕获 useMouseDownToMove.ts

--- web/useMouseDownToMove.ts
+++ web/useMouseDownToMove.ts
@@ -5,15 +5,24 @@ export type Results = {
     ys:Ref<any>
     x:Ref<any>
     y:Ref<any>
+    mx:Ref<any>
+    my:Ref<any>
+    mxs:Ref<any>
+    mys:Ref<any>
     isMousedown:Ref<boolean>
     isMousemove:Ref<boolean>
+    isMMousedown:Ref<boolean>
     buttonType:Ref<any>
 }
+export type MouseEventType = MouseEvent & {
+    path:HTMLElement[]
+}
 export type UseMouseDownToMove = (target?:TargetType<HTMLElement | Window>, options?:Partial<{
-    onMousedown(result:Results, ev:MouseEvent, key:number):void
-    onMousemove(result:Results, ev:MouseEvent, key:number):void
-    onMouseup(result:Results, ev:MouseEvent, key:number):void
-    isMousedown(result:Results, ev:MouseEvent, key:number):boolean
+    onMousedown(result:Results, ev:MouseEventType, key:number):void
+    onMousemove(result:Results, ev:MouseEventType, key:number):void
+    onMouseup(result:Results, ev:MouseEventType, key:number):void
+    onContextmenu(result:Results, ev:MouseEventType, key:number):void
+    isMousedown(result:Results, ev:MouseEventType, key:number):boolean
     key:number
 }>, isInit?:boolean)=> Results;
 
@@ -25,8 +34,13 @@ const useMouseDownToMove:UseMouseDownToMove = (target:any = window, options = {}
     const ys = ref<any>(0)
     const x = ref<any>(0)
     const y = ref<any>(0)
+    const mx = ref<any>(0)
+    const my = ref<any>(0)
+    const mxs = ref<any>(0)
+    const mys = ref<any>(0)
     const buttonType = ref<any>(0)
     const isMousedown = ref<any>(false)
+    const isMMousedown = ref<any>(false)
     const isMousemove = ref<any>(false)
     const result:Results = {
         xs,
@@ -34,54 +48,64 @@ const useMouseDownToMove:UseMouseDownToMove = (target:any = window, options = {}
         x,
         y,
         isMousedown,
+        mx,
+        my,
+        mxs,
+        mys,
+        isMMousedown,
         isMousemove,
         buttonType
     }
     const mousedown = (ev:any) => {
-        isMousemove.value = false
         buttonType.value = ev.button
+        isMousemove.value = false
+        isMMousedown.value = true
         const bool = options.isMousedown?.(result, ev, options.key as number)
         isMousedown.value = typeof bool === 'boolean' ? bool : true
         if (isMousedown.value) {
             xs.value = ev.layerX
             ys.value = ev.layerY
-            options.onMousedown?.(result, ev, options.key as number)
         }
+        if (isMMousedown.value) {
+            mxs.value = ev.layerX
+            mys.value = ev.layerY
+        }
+        options.onMousedown?.(result, ev, options.key as number)
     }
     const mousemove = (ev:any) => {
         isMousemove.value = true
+        if (isMMousedown.value) {
+            mx.value = ev.layerX - mxs.value
+            my.value = ev.layerY - mys.value
+        }
         if (isMousedown.value) {
             x.value = ev.layerX - xs.value
             y.value = ev.layerY - ys.value
-            options.onMousemove?.(result, ev, options.key as number)
         }
+        options.onMousemove?.(result, ev, options.key as number)
     }
     const mouseup = (ev:any) => {
+        isMMousedown.value = false
         isMousedown.value = false
         isMousemove.value = false
         options.onMouseup?.(result, ev, options.key as number)
         nextTick(() => {
-            x.value = 0
-            y.value = 0
             xs.value = 0
             ys.value = 0
+            x.value = 0
+            y.value = 0
+            mx.value = 0
+            my.value = 0
+            mxs.value = 0
+            mys.value = 0
         })
     }
     const init = () => {
-        if (Object.prototype.toString.call(target.value) === '[object Array]') {
-            (target.value as HTMLElement[]).map((t, key) => {
-                const res = useMouseDownToMove(t, {...options, key}, true)
-                xs.value[key] = res.xs.value
-                ys.value[key] = res.ys.value
-                x.value[key] = res.x.value
-                y.value[key] = res.y.value
-                isMousedown.value[key] = res.isMousedown.value
-            })
-            return
-        } else {
-            (target.value as HTMLElement).addEventListener('mousedown', mousedown);
-            (target.value as HTMLElement).addEventListener('mousemove', mousemove);
-            (target.value as HTMLElement).addEventListener('mouseup', mouseup)
+        (target.value as HTMLElement).addEventListener('mousedown', mousedown);
+        (target.value as HTMLElement).addEventListener('mousemove', mousemove);
+        (target.value as HTMLElement).addEventListener('mouseup', mouseup);
+        (target.value as HTMLElement).oncontextmenu = (evt:any) => {
+            return options.onContextmenu?.(result, evt, options.key as number)
         }
     }
     if (isInit) {
