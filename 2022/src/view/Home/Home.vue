29a32722878a0e090aab415374485f1cd2a63693
zys8119 <770959294@qq.com>
2022-12-30 17:38:46 +0800

    [add] 年终总结

--- 2022/src/view/Home/Home.vue
+++ 2022/src/view/Home/Home.vue
@@ -1,19 +1,33 @@
 <template>
-    <div ref="container" class="Home">
+    <div ref="container" class="Home reveal">
         <div class="slides">
-            <section>Horizontal Slide</section>
-            <section>
-                <section>Vertical Slide 1</section>
-                <section>Vertical Slide 2</section>
+            <section data-markdown data-background-color="aquamarine">
+                <textarea data-template>
+                    ## Slide 1
+                    A paragraph with some text and a [link](http://hakim.se).
+                    # asdaswd
+                    > asdasdad
+                    ```
+                    asdasda
+                    ```
+                    ---
+                    ## Slide 2
+                    ---
+                    ## Slide 3asdasdaasdasdasdasdas
+                  </textarea>
             </section>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+// https://revealjs.com/markup/
 import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
 const container = ref()
+Reveal.on( 'make-it-pop', () => {
+    console.log('✨')
+} )
 onMounted(() => {
     const deck = new Reveal(container.value, {
         plugins: [ Markdown ],
@@ -24,5 +38,15 @@ onMounted(() => {
 
 <style scoped lang="less">
 .Home {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    :deep(.reveal){
+        .make-it-pop {
+            filter: drop-shadow(0 0 10px purple);
+        }
+    }
 }
 </style>
