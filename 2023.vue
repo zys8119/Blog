4553f40bb86464422fe910beee62f918c06247ad
zys8119 <770959294@qq.com>
2023-01-05 17:08:41 +0800

    [add] 演示模版

---
+++ 2023.vue
@@ -0,0 +1,196 @@
+<h1 class="header">
+<img width="50" height="50" src="https://avatars.githubusercontent.com/u/19203342?v=4" alt="">
+<span>2023年工作总结与规划@张云山</span>
+</h1>
+
+<nav>
+<div index="0" class="active">工作概览</div>
+<div index="1">工作完成情况</div>
+<div index="2">年度工作计划</div>
+<div index="3">工作思考与感悟</div>
+</nav>
+
+<section>
+<h1 class="fragment">工作概览</h1>
+
+<div class="fragment">一年的光阴一闪而过。在过去的一年里，使自身能力得到更好的发挥与锻炼及提升，同时也离不开智加团队的每一个给予我帮助的人，在此很感谢智加（家）！希望在已经到来的2023年度里面，自我可以有更好的发挥及成就。</div>
+<h2 class="fragment">项目回顾</h2>
+
+<div class="fragment">回顾这段时间，我在智加团队，努力完成了</div>
+<br>
+<table class="fragment">
+    <thead>
+    <tr>
+        <th>项目名称</th>
+        <th>项目描述</th>
+        <th>项目进度</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>node-serve/前端低代码平平台 </td>
+        <td>属于智加团队的node服务框架及前端低代码开发平台</td>
+        <td>100%/开发中</td>
+    </tr>
+    <tr>
+        <td>前端日志监控平台 </td>
+        <td>vue+nodesjs的前端监控平台，集错误收集分析；资源监测；性能分析；用户分析和跟踪；远程代码调试一体滑平台</td>
+        <td>100%</td>
+    </tr>
+    <tr>
+        <td>项目维护</td>
+        <td>代码混淆加密、微前端技术融合、人大模块维护等</td>
+        <td>100%</td>
+    </tr>
+    <tr>
+        <td>wisdom-plus/vue3框架 </td>
+        <td>全面拥抱vite+vue3促进组员的技术延伸</td>
+        <td>100%</td>
+    </tr>
+    </tbody>
+</table>
+</section>
+
+<section>
+<h1>工作完成情况</h1>
+<div class="r-stack">
+    <div class="fragment fade-in-then-out">
+        <h2>node-serve/低代码平台</h2>
+        <h3>node-serve</h3>
+        <div>封装属于智加前端团队的node服务框架，以零依赖完全可控的方案构建，尽可能降低现阶段前端资源的安全隐患</div>
+        <h3>低代码平台</h3>
+        <div>实现以拖拽方式快速搭建项目，默认集成日志监控平台，以降低对大屏项目的繁琐与重复的工作提升工作效率</div>
+        <h3>目标</h3>
+        <div>完全自主可控并形成智加前端团队体系化的项目架构和技术底蕴，以实现后续的敏捷开发模式给以完善的支持</div>
+    </div>
+    <div class="fragment fade-in-then-out">
+        <h2>前端日志监控平台</h2>
+        <h3>数据上报</h3>
+        <div>通过产品分类，独立产品，对产品的数据进行隔离 前端绑定对应产品id并上传相关数据</div>
+        <h3>数据分析</h3>
+        <div>对前端数据进行细致化后形成可视化界面并可以关联通知模块即时的发出预警报告</div>
+        <h3>目标</h3>
+        <div>解决目前团队项目的日志空缺，完善项目的代码追踪，排错及调试；以解决以往h5项目的问题排查难，频繁发包问题</div>
+    </div>
+    <div class="fragment fade-in-then-out">
+        <h2>项目维护</h2>
+        <div>对现有及日常新功能的研发和维护，保证项目的安全平稳运行，
+            完善往期项目的代码混淆加密、现项目微前端技术融合解决项目的耦合、人大模块维护等
+        </div>
+    </div>
+    <div class="fragment fade-in-then-out">
+        <h2>wisdom-plus/vue3框架</h2>
+        <h3>01</h3>
+        <div>采用vite+vue3+gulp等技术，搭建基于vue3 的ui库及开箱即用的前端框架</div>
+        <h3>02</h3>
+        <div>配套的文档支撑及开发模式</div>
+        <h3>03</h3>
+        <div>基于typesrcipt的类型增强以便提升代码质量</div>
+        <h3>04</h3>
+        <div>自动生成多版本，多环境的兼容的构建输出</div>
+    </div>
+</div>
+</section>
+
+<section>
+<h1>2023年工作计划</h1>
+<div>新的一年已经到来，不忘初心，展望未来，玉兔东升。</div>
+<br>
+<div class="r-stack">
+    <div class="fragment fade-in-then-out">
+        <h3>01、向团队共享当下前端环境，分享学习，提供全站化技术路线，搭建知识学习平台</h3>
+        <div>一个人的团队顶多是孤军奋战；多人团队才能实现战术上的进攻</div>
+    </div>
+    <div class="fragment fade-in-then-out">
+        <h3>02、标准化的前端体系</h3>
+        <div>完善目前项目的不足，补全开发规范及文档空缺，全面拥抱为微服务对项目的模块化解藕</div>
+    </div>
+</div>
+</section>
+
+<section>
+<h1>工作思考感悟</h1>
+<div>没有运动，生活等于昏睡，没有思考，生活等于盲从，没有节制，生活等于毁灭，没有快乐，生活等于凝固。</div>
+<br>
+<div class="r-stack">
+    <div class="fragment fade-in-then-out">
+        <h3>01</h3>
+        <div> 我们每个人都需要工作，我想工作应该是一种创造的过程，创造自身价值的过程，在创造中寻找乐趣和意义才是工作的最高境界。</div>
+    </div>
+    <div class="fragment fade-in-then-out">
+        <h3>02</h3>
+        <div>舞台再大，不上台始终是观众。</div>
+    </div>
+    <div class="fragment fade-in-then-out">
+        <h3>03</h3>
+        <div>我不能摇摆明天和意外到来的顺序，唯有不忘初心，把眼前的事情做好就好，敬这一刻的自己</div>
+    </div>
+</div>
+</section>
+
+<section>
+<h1>THANKS</h1>
+<div>2023年度工作总结与规划  -  张云山</div>
+<br>
+<div>2023年1月</div>
+</section>
+
+
+<style scoped lang="less">
+.header {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    gap: 15px;
+}
+.header * {
+    margin: 0;
+    padding: 0;
+}
+.header span{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.header img{
+    width: 50px;
+    height: 50px;
+    overflow: hidden;
+    border-radius: 100%;
+}
+nav{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+}
+nav>*{
+    cursor: pointer;
+}
+nav>*.active{
+    color:#c76c47 ;
+}
+nav>*:hover{
+    color: #c76c47;
+}
+.slides{
+    display: flex;
+    flex-direction: column;
+}
+.slides *{
+    pointer-events: initial;
+}
+.backgrounds{
+    display: none;
+}
+section{
+    background:#181c23;
+    flex: 1;
+    position: initial !important;
+    margin-top: 15px;
+    padding: 15px;
+    width: calc(100% - 30px) !important;
+    border-radius: 10px;
+    overflow-x: hidden;
+}
+</style>
